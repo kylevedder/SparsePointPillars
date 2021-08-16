d7982ed5f08b89ae11e48db005c60aa3a4328c1b
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-08-16 17:11:23 -0400

    Removed unbuffer

--- run_slurm_sparse.sh
+++ run_slurm_sparse.sh
@@ -5,4 +5,4 @@ srun --gpus=1\
  --qos=eaton-high\
  --time=72:10:00\
  --partition=eaton-compute \
-bash -c "source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 unbuffer python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_sparse.yml --dataset_path /scratch/kvedder/habitat_sampling --pipeline ObjectDetection"
+bash -c "source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_sparse.yml --dataset_path /scratch/kvedder/habitat_sampling --pipeline ObjectDetection"

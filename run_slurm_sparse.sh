3b9c036cd7486cfe27439b96478a76107744ec28
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-08-16 16:54:31 -0400

    Added source and made executable

--- run_slurm_sparse.sh
+++ run_slurm_sparse.sh
@@ -6,4 +6,4 @@ srun --gpus=1\
  --qos=eaton-high\
  --time=72:10:00\
  --partition=eaton-compute \
-OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_sparse.yml --dataset_path /scratch/kvedder/habitat_sampling --pipeline ObjectDetection
+source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_sparse.yml --dataset_path /scratch/kvedder/habitat_sampling --pipeline ObjectDetection

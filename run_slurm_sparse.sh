f5be797acf25519bba9f3f7338259b79e0b76a2d
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-08-16 16:52:34 -0400

    Added slurm command to run sparse PP

---
+++ run_slurm_sparse.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+srun --gpus=1\
+ --mem-per-gpu=32G\
+ --cpus-per-gpu=16\
+ -w node-3090-1\
+ --qos=eaton-high\
+ --time=72:10:00\
+ --partition=eaton-compute \
+OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_sparse.yml --dataset_path /scratch/kvedder/habitat_sampling --pipeline ObjectDetection

f6c7033037d1c27c3d3fb8e371ff14ef9977887d
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-08-17 11:39:16 -0400

    Added Docker container info

--- run_slurm_dense.sh
+++ run_slurm_dense.sh
@@ -5,4 +5,6 @@ srun --gpus=1\
  --qos=eaton-high\
  --time=36:10:00\
  --partition=eaton-compute \
+ --container-mounts=/scratch:/scratch,/home/kvedder/code/Open3D-ML:/project\
+ --container-image=docker-registry.grasp.cluster#open3dml \
 bash -c "source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling.yml --dataset_path /scratch/kvedder/habitat_sampling --pipeline ObjectDetection"

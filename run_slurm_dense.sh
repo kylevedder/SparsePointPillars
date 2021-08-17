81f6451bf41c2e6a71c098396bdff1b900573394
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-08-17 11:38:00 -0400

    Added sparse and dense run with decreased time use

---
+++ run_slurm_dense.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+srun --gpus=1\
+ --mem-per-gpu=32G\
+ --cpus-per-gpu=16\
+ --qos=eaton-high\
+ --time=36:10:00\
+ --partition=eaton-compute \
+bash -c "source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling.yml --dataset_path /scratch/kvedder/habitat_sampling --pipeline ObjectDetection"
--- run_slurm_sparse.sh
+++ run_slurm_sparse.sh
@@ -3,6 +3,6 @@ srun --gpus=1\
  --mem-per-gpu=32G\
  --cpus-per-gpu=16\
  --qos=eaton-high\
- --time=72:10:00\
+ --time=36:10:00\
  --partition=eaton-compute \
 bash -c "source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_sparse.yml --dataset_path /scratch/kvedder/habitat_sampling --pipeline ObjectDetection"

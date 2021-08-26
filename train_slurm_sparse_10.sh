4dcde4bdf9365906b71973f79cc1a89800353459
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-08-25 22:18:20 -0400

    Added nodelist

--- train_slurm_sparse_10.sh
+++ train_slurm_sparse_10.sh
@@ -5,6 +5,7 @@ srun --gpus=1\
  --qos=eaton-high\
  --time=48:10:00\
  --partition=eaton-compute \
+ --nodelist=node-3090-[1-2]\
  --container-mounts=/scratch:/scratch,/home/kvedder/code/Open3D-ML:/project\
  --container-image=docker-registry.grasp.cluster#open3dml \
 bash -c "source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_sparse_10.yml --dataset_path /scratch/kvedder/habitat_sampling_large --pipeline ObjectDetection"

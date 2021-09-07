2d6c846833f5593f1d1f6f5906d2158123eb88bf
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-09-07 10:10:16 -0400

    Fixed dense train config that killed my job :(

--- train_slurm_dense_01_wd_4_5000_mean_box.sh
+++ train_slurm_dense_01_wd_4_5000_mean_box.sh
@@ -8,4 +8,4 @@ srun --gpus=1\
  --exclude=node-1080ti-3\
  --container-mounts=/scratch:/scratch,/home/kvedder/code/Open3D-ML:/project\
  --container-image=docker-registry.grasp.cluster#open3dml \
-bash -c "source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_dense_01_wd_4_5000_2x2_mean_box.yml --dataset_path /scratch/kvedder/habitat_sampling_large --pipeline ObjectDetection"
+bash -c "source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_dense_01_wd_4_mean_box.yml --dataset_path /scratch/kvedder/habitat_sampling_large --pipeline ObjectDetection"

68c00f7dabf629916e869afb944e1656528c0c27
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-09-07 15:22:38 -0400

    Added /Datasets mount

--- train_slurm_kitti_dense.sh
+++ train_slurm_kitti_dense.sh
@@ -5,6 +5,6 @@ srun --gpus=1\
  --qos=eaton-high\
  --time=48:10:00\
  --partition=eaton-compute \
- --container-mounts=/scratch:/scratch,/home/kvedder/code/Open3D-ML:/project\
+ --container-mounts=/scratch:/scratch,/Datasets:/Datasets,/home/kvedder/code/Open3D-ML:/project\
  --container-image=docker-registry.grasp.cluster#open3dml \
 bash -c "source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_kitti_car_only.yml --dataset_path /Datasets/kitti_object_detect/ --pipeline ObjectDetection"

570413215fb87231ffc1a06e007acf45c45531d3
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-09-09 15:28:35 -0400

    Added xavier dense and sparse test scripts

---
+++ test_xavier_sparse_habitat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#!/bin/bash
+x=1
+while [ $x -le 10 ]
+do
+  echo "Step $x"
+  source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_sparse_5000_2x2_mean_box.yml --dataset_path /data/habitat_sampling_dataset --pipeline ObjectDetection --split test --ckpt_path=./logs_sparse_5000_2x2_mean_box/SparsePointPillars_HabitatSampling_torch/checkpoint/ckpt_00200.pth
+  x=$(( $x + 1 ))
+done

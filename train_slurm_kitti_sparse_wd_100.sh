19e618968fdda95139d38fd9c0239b2b284036ee
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-09-08 23:26:18 -0400

    Added experiments with learning rate and weight decay

---
+++ train_slurm_kitti_sparse_wd_100.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+srun --gpus=1\
+ --mem-per-gpu=20G\
+ --cpus-per-gpu=12\
+ --qos=eaton-high\
+ --time=48:10:00\
+ --partition=eaton-compute \
+ --container-mounts=/scratch:/scratch,/Datasets:/Datasets,/home/kvedder/code/Open3D-ML:/project\
+ --container-image=docker-registry.grasp.cluster#open3dml \
+bash -c "source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/sparse_pointpillars_kitti_car_only_wd_100.yml --dataset_path /Datasets/kitti_object_detect/ --pipeline ObjectDetection"

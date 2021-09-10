8111e324560c35bb646f132752218acf3b8a711e
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-09-09 23:01:17 -0400

    Reduced number of iterations on CPU

--- test_robot_dense_habitat.sh
+++ test_robot_dense_habitat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #!/bin/bash
 x=1
-while [ $x -le 10 ]
+while [ $x -le 3 ]
 do
   echo "Step $x"
   source set_open3d_ml_root.sh; OMP_NUM_THREADS=`nproc` python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_dense_5000_mean_box.yml --dataset_path /data/habitat_sampling_dataset --pipeline ObjectDetection --split test --ckpt_path=./logs_dense_5000_mean_box/PointPillars_HabitatSampling_torch/checkpoint/ckpt_00200.pth --device cpu

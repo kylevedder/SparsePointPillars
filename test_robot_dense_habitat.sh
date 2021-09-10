cfd9dfda427762f2e5eed9a4f23e42aed1dc108b
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-09-09 22:58:55 -0400

    Added nproc for OMP

--- test_robot_dense_habitat.sh
+++ test_robot_dense_habitat.sh
@@ -4,6 +4,6 @@ x=1
 while [ $x -le 10 ]
 do
   echo "Step $x"
-  source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_dense_5000_mean_box.yml --dataset_path /data/habitat_sampling_dataset --pipeline ObjectDetection --split test --ckpt_path=./logs_dense_5000_mean_box/PointPillars_HabitatSampling_torch/checkpoint/ckpt_00200.pth --device cpu
+  source set_open3d_ml_root.sh; OMP_NUM_THREADS=`nproc` python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_habitat_sampling_dense_5000_mean_box.yml --dataset_path /data/habitat_sampling_dataset --pipeline ObjectDetection --split test --ckpt_path=./logs_dense_5000_mean_box/PointPillars_HabitatSampling_torch/checkpoint/ckpt_00200.pth --device cpu
   x=$(( $x + 1 ))
 done

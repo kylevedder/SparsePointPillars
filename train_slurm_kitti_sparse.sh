08baee235f12eb62da8e5c2b97c6d686aebc04ef
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-09-11 13:23:24 -0400

    Added time commands to sparse and dense KITTI train.

--- train_slurm_kitti_sparse.sh
+++ train_slurm_kitti_sparse.sh
@@ -7,4 +7,4 @@ srun --gpus=1\
  --partition=eaton-compute \
  --container-mounts=/scratch:/scratch,/Datasets:/Datasets,/home/kvedder/code/Open3D-ML:/project\
  --container-image=docker-registry.grasp.cluster#open3dml \
-bash -c "source set_open3d_ml_root.sh; OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/sparse_pointpillars_kitti_car_only.yml --dataset_path /Datasets/kitti_object_detect/ --pipeline ObjectDetection"
+bash -c "source set_open3d_ml_root.sh; apt install -y time; time OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/sparse_pointpillars_kitti_car_only.yml --dataset_path /Datasets/kitti_object_detect/ --pipeline ObjectDetection"

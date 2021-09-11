2a6b1ff02917521eede5b305795dc26bb73c7bda
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-09-11 18:45:34 -0400

    Added nuscenes experiments

---
+++ train_slurm_nuscenes_dense.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+srun --gpus=1\
+ --mem-per-gpu=20G\
+ --cpus-per-gpu=12\
+ --qos=eaton-high\
+ --time=48:10:00\
+ --partition=eaton-compute \
+ --container-mounts=/scratch:/scratch,/Datasets:/Datasets,/home/kvedder/code/Open3D-ML:/project\
+ --container-image=docker-registry.grasp.cluster#open3dml \
+bash -c "source set_open3d_ml_root.sh; apt install -y time; time OMP_NUM_THREADS=1 python scripts/run_pipeline.py torch -c ml3d/configs/pointpillars_nuscenes.yml --dataset_path /Datasets/nuscenes/ --pipeline ObjectDetection"
